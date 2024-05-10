80403ed25d522a911cd1f23a24552bdd8f44b2a3
Charles Shi <schrht@gmail.com>
2024-05-09 21:29:58 -0400

    Add run_self_test.sh

---
+++ run_self_test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Description:
+#   Trigger the Block Device self-test.
+# Maintainer:
+#   Charles Shi <schrht@gmail.com>
+
+show_usage() {
+	echo "Usage:   ${BASH_SOURCE[0]} <block_device> [short|long]"
+	echo "Example: ${BASH_SOURCE[0]} /dev/sdx short"
+}
+
+[[ -z $1 ]] && show_usage && exit 1
+if [[ -b $1 ]]; then
+	dev=$1
+else
+	echo "$1 is not a block device!"
+	exit 1
+fi
+
+[[ -n $2 ]] && type=$2 || type=short
+
+# Trigger the self-test
+if sudo smartctl -t "$type" "$dev"; then
+	echo -e "\nThe self-test for '$dev' has been triggered."
+	echo -e "Run 'sudo smartctl -l selftest ${dev}' to get the report by then."
+	watch -c -d -b -e -n 5 sudo smartctl -l selftest "${dev}"
+	exit 0
+else
+	echo -e "\nFail to trigger the self-test."
+	exit 1
+fi
+
